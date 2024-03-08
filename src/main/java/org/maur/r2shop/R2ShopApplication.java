edf23b9afc001625e91711be9d5b7a4cf81fb736
Giang Dinh Hoang <21880030@student.hcmus.edu.vn>
2024-03-08 15:06:29 +0700

    initial project

---
+++ src/main/java/org/maur/r2shop/R2ShopApplication.java
@@ -0,0 +1,13 @@
+package org.maur.r2shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class R2ShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(R2ShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/maur/r2shop/R2ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.maur.r2shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class R2ShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
